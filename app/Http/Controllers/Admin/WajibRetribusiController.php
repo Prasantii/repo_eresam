52639b648ddfe5f4f73e6b86fa5f161573251c77
ariand <mriskiariandaa@gmail.com>
2021-12-06 10:53:08 +0700

    import coba2

--- app/Http/Controllers/Admin/WajibRetribusiController.php
+++ app/Http/Controllers/Admin/WajibRetribusiController.php
@@ -1361,20 +1361,35 @@ class WajibRetribusiController extends Controller
         
     }
 
-    public function importGampong2(Request $request)
+    public function importGampong(Request $request)
     {
+        // set waktu agar tidak dibatasi oleh laravel
+        set_time_limit(0);
+        // validasi
         $this->validate($request, [
-        'file' => 'required|mimes:csv,xlsx,xls'
+            'file' => 'required|mimes:csv,xls,xlsx'
         ]);
+ 
+        // menangkap file excel
         $file = $request->file('file');
+ 
+        // membuat nama file unik
         $nama_file = rand().$file->getClientOriginalName();
-        $file->move('uploads/import',$nama_file);
-        Excel::import(new wrGampong_import, public_path('/uploads/import/'));
-        Session::flash('sukses','Data Siswa Berhasil Diimport!');
+ 
+        // upload ke folder file_siswa di dalam folder public
+        $file->move('import',$nama_file);
+ 
+        // import data
+        Excel::import(new wrGampongimport, public_path('/uploads/import/'.$nama_file));
+
+        // notifikasi dengan session
+        Session::flash('sukses','Data Berhasil Diimport!');
+ 
+        // alihkan halaman kembali
         return redirect('devadmin/wajib_retribusi');
     }
     
-    public function importGampong(Request $request)
+    public function importGampong2(Request $request)
     {
         // Excel::import(new wrGampong_import, request()->file('file'));
         // return back();
--- app/Imports/wrGampongimport.php
+++ app/Imports/wrGampongimport.php
@@ -5,9 +5,11 @@ namespace App\Imports;
 use App\wrGampong;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use maatwebsite\Excel\Concerns\WithChunkReading;
+use Illuminate\Contracts\Queue\ShouldQueue;
 
 
-class wrGampongimport implements ToModel, WithHeadingRow
+class wrGampongimport implements ToModel, WithChunkReading,ShouldQueue ,WithHeadingRow
 {
     /**
     * @param array $row
@@ -17,13 +19,26 @@ class wrGampongimport implements ToModel, WithHeadingRow
     public function model(array $row)
     {
         return new wrGampong([
-            'code' =>$row["code"],
-            'nik'  =>$row["nik"],
-            'nama' =>$row["nama"],
-            'alamat' =>$row["alamat"],
-            'jenis_retribusi' =>$row["jenis_id"],
-            'tarif_gampong' =>$row["tarif_gampong"],
-            'is_active' =>$row["is_active"]
+
+            'code' => $row[1],
+            'nik'   => $row[2],
+            'nama'   => $row[3],
+            'alamat'    => $row[4],
+            'jenis_id' =>$row[5],
+            'is_active' => $row[6]
+
+            // 'code' =>$row["code"],
+            // 'nik'  =>$row["nik"],
+            // 'nama' =>$row["nama"],
+            // 'alamat' =>$row["alamat"],
+            // 'jenis_retribusi' =>$row["jenis_id"],
+            // 'tarif_gampong' =>$row["tarif_gampong"],
+            // 'is_active' =>$row["is_active"]
         ]);
+    }
+
+    public function chunkSize(): int
+    {
+        return 1000;
     }
 }
